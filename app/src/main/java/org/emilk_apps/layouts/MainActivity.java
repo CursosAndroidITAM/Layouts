6a798194577497609ff1febfd7baa1fe266b433f
emirua <emirua@hotmail.com>
2016-09-28 21:33:46 -0500

    Actividades

--- app/src/main/java/org/emilk_apps/layouts/MainActivity.java
+++ app/src/main/java/org/emilk_apps/layouts/MainActivity.java
@@ -1,33 +1,50 @@
 package org.emilk_apps.layouts;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
     private static String MAINACTIVITY = "mainActivity";
-    private TextView myTextView;
+    @BindView(R.id.activity_tv)
+    TextView activityTv;
+    @BindView(R.id.activity_button)
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        myTextView = (TextView) findViewById(R.id.activity_tv);
-        myTextView.setText("Este ya no es un Hola Mundo");
-        Button button = (Button) findViewById(R.id.activity_button);
+        setContentView(R    .layout.activity_main);
+        ButterKnife.bind(this);
+        activityTv.setText("Este ya no es un Hola Mundo");
         button.setText("Algo más");
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"Click",Toast.LENGTH_LONG).show();
+                showSecondActivity();
             }
         });
 
+    }
+
+    private void showSecondActivity() {
+        Intent i = new Intent(this, SecondActivity.class);
+        i.putExtra("EXTRA", "dato extra");
+        startActivity(i);
+
+    }
+
+    private void showOtherActivity() {
+        Intent i = new Intent(this, OtherActivity.class);
+        startActivity(i);
     }
 
     @Override
